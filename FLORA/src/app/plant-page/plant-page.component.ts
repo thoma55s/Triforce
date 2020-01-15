6e0086d2dd176874a29e947e281fbaf44562587f
Thomas <thomas.morgan.sager@gmail.com>
2020-01-14 20:14:34 -0500

    It work

--- FLORA/src/app/plant-page/plant-page.component.ts
+++ FLORA/src/app/plant-page/plant-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ItemEventData, ListView } from "tns-core-modules/ui/list-view";
+import { HttpClient } from '@angular/common/http';
+import { ApiKey } from '../shared/api-key';
 
 
 @Component({
@@ -7,14 +9,12 @@ import { ItemEventData, ListView } from "tns-core-modules/ui/list-view";
   templateUrl: './plant-page.component.html',
   styleUrls: ['./plant-page.component.css']
 })
-export class PlantPageComponent implements OnInit {
+export class PlantPageComponent {
 
-    plants : Array<Plant>
-  constructor() { }
+  test: string;
+  plants: Plant[];
 
-  ngOnInit() {
-    this.plants = JSON.parse('[{"Name" : "rose","ImageUrl" : "https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2F3.bp.blogspot.com%2F-RHTc0HJizio%2FUZ4b_u08BQI%2FAAAAAAAAPQk%2Fg21ku1sURIk%2Fs1600%2Fred-rose154388.jpg&f=1&nofb=1"}, {"Name" : "tulip","ImageUrl" : "https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.luOE41r0HjCOmcqobg6mxwHaHa%26pid%3DApi&f=1"}]');
-    }
-
-
+  constructor(private http: HttpClient) {
+    http.get(ApiKey.url).subscribe(x => { this.plants = JSON.parse(JSON.stringify(x)); });
+  }
 }
