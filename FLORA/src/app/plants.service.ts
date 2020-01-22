867a9b9e1fcd4a706ff44a5fdce18a2e8f398730
HannahBloyd <hannahbloyd15@gmail.com>
2020-01-22 12:00:17 -0500

    remove unnecessary code

--- FLORA/src/app/plants.service.ts
+++ FLORA/src/app/plants.service.ts
@@ -1,17 +1,9 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlantsService {
 
-  private messageSource = new BehaviorSubject('default message');
-  currentMessage = this.messageSource.asObservable();
-
   constructor() { }
-
-  changeMessage(message: string) {
-    this.messageSource.next(message)
-  }
 }
