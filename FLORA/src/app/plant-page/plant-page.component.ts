cc03c928a6393fc199d000d8dab17b9b695628de
HannahBloyd <hannahbloyd15@gmail.com>
2020-02-27 20:04:42 -0500

    stuff

--- FLORA/src/app/plant-page/plant-page.component.ts
+++ FLORA/src/app/plant-page/plant-page.component.ts
@@ -6,7 +6,7 @@ import { ApiKey } from '../shared/api-key';
 import { forkJoin } from 'rxjs';
 import { Observable } from 'tns-core-modules/ui/page/page';
 import { PlantsService } from '../plants.service';
-import {Router} from "@angular/router"
+import {Router, ActivatedRoute} from "@angular/router"
 
 
 @Component({
@@ -20,34 +20,48 @@ export class PlantPageComponent implements OnInit{
     plants: Plant[];
     goodPlants: Plant[] = [];
     message: string;
+    page = 0;
+    endReached = false;
+    rpp = 50;
 
-    constructor(private http: HttpClient, private data: PlantsService) {}
+    constructor(private http: HttpClient, private data: PlantsService, private route: ActivatedRoute) {}
     apiKey = new ApiKey;
 
     ngOnInit() {
-        this.http.get<Plant[]>(ApiKey.url)
-            .subscribe(x => {
-                this.plants = x;
-                const plantBuilder = [];
-                for (let i = 0; i < this.plants.length; i++) {
-                    const y = this.http.get(this.apiKey.getImageQuery(this.plants[i]));
-                    plantBuilder.push(y);
 
-                }
-                forkJoin(plantBuilder).subscribe(y => {
-                    for (let i = 0; i < plantBuilder.length; i++) {
-                        const res = JSON.parse(JSON.stringify(y[i]));
-                        if (res.hits) {
-                            if (this.plants[i] && res.hits[0]) {
-                                this.plants[i].imageUrl = res.hits[0].largeImageURL;
-                                this.goodPlants.push(this.plants[i]);
-                            }
-                        }
+        // this.page = this.route.snapshot.paramMap.get("page");
+           this.http.get<Plant[]>(/*ApiKey.url*/this.apiKey.pagedUrl(this.page))
+               .subscribe(x => {
+                   this.plants = x;
+                   const plantBuilder = [];
+                   if (this.plants.length < this.rpp){
+                       this.endReached = true;
+                   }
+                   console.log("Plants length " + this.plants.length);
+                   for (let i = 0; i < this.plants.length; i++) {
+                       //console.log(this.plants[i]);
+                       const y = this.http.get(this.apiKey.getImageQuery(this.plants[i]));
+                       plantBuilder.push(y);
                     }
-                })
+                   forkJoin(plantBuilder).subscribe(y => {
+                       //console.log(plantBuilder.length);
+                       for (let i = 0; i < plantBuilder.length; i++) {
+                           //console.log(plantBuilder[i]);
+                           const res = JSON.parse(JSON.stringify(y[i]));
+                           if (res.hits) {
+                               //console.log("res hits = " + res.hits);
+                               if (this.plants[i] && res.hits[0]) {
+                                   this.plants[i].imageUrl = res.hits[0].largeImageURL;
+                                   this.goodPlants.push(this.plants[i]);
+                                   console.log(i);
+                               }
+                           }
+                       }
+                       //console.log("Plants length " + this.plants.length);
+                       //console.log("Good plants length " + this.goodPlants.length);
+                   })
             });
     }
-
 }
 
 
